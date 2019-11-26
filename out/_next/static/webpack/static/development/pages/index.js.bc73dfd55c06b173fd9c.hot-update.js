01dde65e0134ed7ba4364a914c378975df1a177a
aotuotage <826931037@qq.com>
2019-11-26 14:25:31 +0800

    122121

---
+++ out/_next/static/webpack/static/development/pages/index.js.bc73dfd55c06b173fd9c.hot-update.js
@@ -0,0 +1,86 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/router */ "./node_modules/next/dist/client/router.js");
+/* harmony import */ var next_router__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_router__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "/Users/mac/Desktop/next/next-create/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+var Home = function Home() {
+  return __jsx(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "\u6211\u662F\u9996\u9875"), __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: this
+  }, __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/xiaojiejie?name=\u6CE2\u591A\u91CE\u7ED3\u8863",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, __jsx("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "\u9009\u6CE2\u591A\u91CE\u7ED3\u8863")), __jsx("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }), __jsx(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/xiaojiejie?name=\u82CD\u4E95\u7A7A",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, "\u9009\u82CD\u4E95\u7A7A"))));
+};
+
+next_router__WEBPACK_IMPORTED_MODULE_2___default.a.events.on('routeChangeStart', function () {
+  var _console;
+
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  (_console = console).log.apply(_console, ['1.routeChangeStart->路由开始变化,参数为:'].concat(args));
+});
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.bc73dfd55c06b173fd9c.hot-update.js.map
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+const Home = () => {
+  return(
+    <>
+      <div>我是首页</div>
+      <div>
+        <Link href="/xiaojiejie?name=波多野结衣"><a>选波多野结衣</a></Link><br/>
+        <Link href="/xiaojiejie?name=苍井空"><a>选苍井空</a></Link>
+      </div>
+    </>
+  )
+
+}
+Router.events.on('routeChangeStart',(...args)=>{
+  console.log('1.routeChangeStart->路由开始变化,参数为:',...args)
+})
+export default Home
